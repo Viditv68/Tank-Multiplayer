55ff2eeb09931d7d32d8426da205b8c9cdc2d1d8
ViditRawat <vidit.r@cymplstudios.com>
2023-10-14 17:37:02 +0530

    player movement

--- Assets/Scripts/ConnectionButtons.cs
+++ Assets/Scripts/ConnectionButtons.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class JoinServer : MonoBehaviour
+public class ConnectionButtons : MonoBehaviour
 {
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
     public void Join()
     {
         NetworkManager.Singleton.StartClient();
---
+++ Assets/Scripts/Core/Player/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [Header("References")]
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private Transform bodyTransform;
+    [SerializeField] private Rigidbody2D rigidBody;
+
+    [Header("Settings")]
+    [SerializeField] private float movementSpeed = 4f;
+    [SerializeField] private float turningRate = 30f;
+
+
+    private Vector2 previousMovementInput;
+    public override void OnNetworkSpawn()
+    {
+        if(!IsOwner) 
+            return;
+
+        inputReader.MoveEvent += HandleMove;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsOwner)
+            return;
+    }
+
+    void Update()
+    {
+        if (!IsOwner)
+            return;
+
+        float zRotation = previousMovementInput.x * -turningRate * Time.deltaTime;
+
+        bodyTransform.Rotate(0f, 0f, zRotation);
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (!IsOwner)
+            return;
+
+        rigidBody.velocity = (Vector2)bodyTransform.up * previousMovementInput.y  * movementSpeed;
+    }
+
+    private void HandleMove(Vector2 _movementInput)
+    {
+        previousMovementInput = _movementInput;
+    }
+
+}
